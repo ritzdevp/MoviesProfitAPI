e96151812819bf12ff69f3376d45f6c001c7c4b1
Rituraj Singh <rtjsingh30@gmail.com>
2020-07-07 13:12:29 +0530

    created movies class, app route

--- app.py
+++ app.py
@@ -5,3 +5,17 @@ import time
 from datetime import datetime
 
 app = Flask(__name__)
+
+listofmovies = []
+class MovieInfo:
+	def __init__(self, name, start, end):
+		self.name = name
+		self.start = start
+		self.end = end
+
+@app.route('/get_max_profit_API', methods = ['GET'])
+def get_max_profit_API():
+	def compareEnds(movieA, movieB):
+		return movieA.end - movieB.end
+
+	
