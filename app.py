f8ee103deb3e424ef751ddd2a86c39ce0f66e1bc
Rituraj Singh <rtjsingh30@gmail.com>
2020-07-07 13:18:21 +0530

    added greedy algo

--- app.py
+++ app.py
@@ -27,3 +27,13 @@ def get_max_profit_API():
 	sortedByEnd = sorted(moviesList, key = functools.cmp_to_key(compareEnds))
 
 	pickedMovies = []
+	j = 0
+	count = 0
+	for i in range(1, len(sortedByEnd)):
+		if (sortedByEnd[i].start >= sortedByEnd[j].end):
+			pickedMovies.append(sortedByEnd[j].name)
+			count = count + 1
+			j = i
+	pickedMovies.append(sortedByEnd[j].name)
+	j = 0
+	return jsonify(Movies = pickedMovies, MaxProfit = count)
