b6964009b8d1118551c09e73a864a1c685c70001
Rituraj Singh <rtjsingh30@gmail.com>
2020-07-07 13:09:02 +0530

    created Flask app

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-#API
+from flask import Flask, request, jsonify
+from flask import render_template, redirect
+import functools, json
+import time
+from datetime import datetime
+
+app = Flask(__name__)
